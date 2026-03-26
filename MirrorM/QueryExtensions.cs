82b954ae0c4e4c047f13ef9a0f9f1f6fc2f33c61
Slava Vodianov <vacheslav@sozap.com>
2026-03-26 13:00:47 +0100

    Optimized some QueryExtensions methods

--- MirrorM/QueryExtensions.cs
+++ MirrorM/QueryExtensions.cs
@@ -10,7 +10,7 @@ namespace MirrorM
     {
         public static async Task<T> FirstAsync<T>(this IQuery<T> query) where T : Entity
         {
-            return (await query.Take(1).ToArrayAsync())[0];
+            return await query.Take(1).FirstOrDefaultAsync() ?? throw new InvalidOperationException();
         }
 
         public static async Task<T> FirstAsync<T>(this IQuery<T> query, Expression<Func<T, bool>> predicate) where T : Entity
@@ -20,7 +20,12 @@ namespace MirrorM
 
         public static async Task<T?> FirstOrDefaultAsync<T>(this IQuery<T> query) where T : Entity
         {
-            return (await query.Take(1).ToArrayAsync()).FirstOrDefault();
+            await using var enumerator = query.Take(1).ToAsyncEnumerable().GetAsyncEnumerator();
+
+            if (await enumerator.MoveNextAsync())
+                return enumerator.Current;
+
+            return null;
         }
 
         public static async Task<T?> FirstOrDefaultAsync<T>(this IQuery<T> query, Expression<Func<T, bool>> predicate) where T : Entity
