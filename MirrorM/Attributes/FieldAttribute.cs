8072f95d35ba14dfc4b13003ffbace73811ede52
Slava Vodianov <vacheslav@sozap.com>
2026-04-17 14:16:20 +0200

    Removed not used parameters from FieldAttribute

--- MirrorM/Attributes/FieldAttribute.cs
+++ MirrorM/Attributes/FieldAttribute.cs
@@ -6,16 +6,10 @@ namespace MirrorM.Attributes
     public class FieldAttribute : Attribute
     {
         public string FieldName { get; }
-        public bool Indexed { get; }
-        public string? TypeOverride { get; }
-        public bool Queriable { get; }
 
-        public FieldAttribute(string value, bool indexed = false, string? typeOverride = null, bool queriable = true)
+        public FieldAttribute(string value)
         {
             FieldName = value;
-            Indexed = indexed;
-            TypeOverride = typeOverride;
-            Queriable = queriable;
         }
     }
 }
